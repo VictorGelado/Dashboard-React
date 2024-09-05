2c358742ef7b1586ae1fe2dcee4a185fbed0645b
VictorGelado <VictorGelado>
2024-09-05 11:15:03 -0300

    Tela 1

--- tela1/src/App.js
+++ tela1/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import menu from './imagens/menu-hamburguer.svg';
+import bell from './imagens/sino.svg';
+import profile from './imagens/imagem1.jpg';
+import {ReactComponent as Carro} from './imagens/carro.svg';
+import {ReactComponent as Coracao} from './imagens/coracao.svg';
+import {ReactComponent as Envelope} from './imagens/envelope.svg';
+import {ReactComponent as Pulso} from './imagens/pulso.svg';
+import {ReactComponent as Acento} from './imagens/acento.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={menu} alt="hamburguer" className="icon-header" />
+
+        <div className="icon-container">
+          <img src={bell} alt="sino" className="icon-header" />
+        </div>
       </header>
+
+      <div className="container-profile">
+        <img src={profile} alt="profile" className="img-profile" />
+        <div className="container-data">
+          <p>Daniel Quite</p>
+          <p>+29 0228 3850</p>
+          <p>Tátá</p>
+        </div>
+      </div>
+
+      <div className="menu-options">
+        <div className="options">
+          <Carro className="img-options" />  
+          <p>Ride</p>
+        </div>
+        <div className="options">
+          <Envelope className="img-options" />  
+          <p>Ride</p>
+        </div>
+        <div className="options">
+          <Coracao className="img-options" />  
+          <p>Ride</p>
+        </div>
+        <div className="options">
+          <Pulso className="img-options" />  
+          <p>Ride</p>
+        </div>
+      </div>
+
+      <p className="new-ride">New ride reguest!</p>
+
+      <div className="container-ride">
+        <Acento className="acento"/>
+
+        <div className='ride'>
+          <div className="data-ride">
+            <p>Jeck, N.</p>
+            <p>aaaaaaaa</p>
+          </div>
+          <img src={profile} alt="imagem" className="profile-image-small"/>
+        </div>
+      </div>
+
+      <div>
+        
+      </div>
+
+
     </div>
   );
 }
